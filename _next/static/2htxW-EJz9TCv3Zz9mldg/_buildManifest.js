3da183e0d96b48e063c98beb11e3eb3643c69cea
hieudoanm <hieumdoan@gmail.com>
2023-02-24 04:11:13 +0000

    Deploying to gh-pages from @ houseofmystery/cafesang@2fca0d5d31ec8f5856d8cd2f083306f7e2cc37ee 🚀

---
+++ _next/static/2htxW-EJz9TCv3Zz9mldg/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/938-9b8114a14769c4a9.js","static/chunks/pages/index-c97409b5e6ff0074.js"],"/_error":["static/chunks/pages/_error-8353112a01355ec2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
