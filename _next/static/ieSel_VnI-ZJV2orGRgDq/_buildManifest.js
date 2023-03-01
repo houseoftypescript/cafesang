1c50655583ab2f5c55b4c4b15d8e5480f890b867
hieudoanm <hieumdoan@gmail.com>
2023-03-01 11:49:16 +0000

    Deploying to gh-pages from @ houseoftypescript/cafesang@54c13c372d63a964a84452057153ce88608663b9 🚀

--- _next/static/ieSel_VnI-ZJV2orGRgDq/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/35-189ff0006f45d2fd.js","static/chunks/pages/index-faf57d1aeb70fbbf.js"],"/_error":["static/chunks/pages/_error-8353112a01355ec2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
